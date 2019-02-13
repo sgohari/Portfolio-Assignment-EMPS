7a2821f5b516af923799f5560b470165bcbd4265
sgohary <nasir_2005@msn.com>
2019-02-13 13:14:35 -0500

    reorganized project structures

--- server/config/app.js
+++ server/config/app.js
@@ -6,7 +6,7 @@ let logger = require('morgan');
 
 //db setup
 let mongoose = require('mongoose');
-let DB=require('./config/db');
+let DB=require('./db');
 
 //point Mongoose to DB URI
 mongoose.connect(DB.URI);
@@ -17,25 +17,25 @@ mongoDB.once('open',()=>{
   console.log('Connected to MongoDB');
 });
 
-let indexRouter = require('./routes/index');
+let indexRouter = require('../routes/index');
 //setting the rout to myFv page
-let contactPage = require('./routes/contact');
-let aboutPage = require('./routes/abouts')
+let contactPage = require('../routes/contact');
+let aboutPage = require('../routes/abouts')
 
 
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 app.use('/', indexRouter);
 
 //pointing the page here
