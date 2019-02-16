bed8df125605aad84f1ba3ceaae2bf0355572075
sgohary <nasir_2005@msn.com>
2019-02-16 18:03:38 -0500

    mlab connection string is restrored

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-   "URI" : "mongodb://localhost/contactme"
-   //"URI":"mongodb://nacer:amin2005@ds050739.mlab.com:50739/contactme"
+   //"URI" : "mongodb://localhost/contactme"
+   "URI":"mongodb://nacer:amin2005@ds050739.mlab.com:50739/contactme"
 }
